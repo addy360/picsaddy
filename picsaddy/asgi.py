5680e298ab346a000a40ce85c58a0a0ea6eb48c0
addy360 <abbyboy360@gmail.com>
2021-07-06 13:58:06 +0300

    initial commit

---
+++ picsaddy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for picsaddy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picsaddy.settings')
+
+application = get_asgi_application()

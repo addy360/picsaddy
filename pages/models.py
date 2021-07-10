a462a6b9f70e915224130858f49675f95e056670
addy360 <abbyboy360@gmail.com>
2021-07-10 19:34:21 +0300

    i think its ready for deploy for now :)

--- pages/models.py
+++ pages/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Blog(models.Model):
     id = models.BigAutoField(primary_key=True)
-    title = models.TextField(max_length=100)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField()
     image = models.FileField(upload_to="blogs")
@@ -24,7 +24,7 @@ class Blog(models.Model):
 class Bio(models.Model):
     id = models.BigAutoField(primary_key=True)
     image = models.FileField(upload_to="biopics")
-    title = models.TextField(max_length=100)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     created_at = models.DateField(auto_now_add=True)
